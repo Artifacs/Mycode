29b8b40511402d674d2ec0206b42d0e0897d1f97
Lesze <lesterandrew.arollado.cvt@eac.edu.ph>
2026-08-12 22:50:20 +0800

    Reapply "Git revert experiment"
    This reverts commit ecba524e651267e1a33db5f065c744f927895139.

--- src/App.js
+++ src/App.js
@@ -6,5 +6,16 @@ import './App.css';
 import './firebase';
 
 function App() {
+<<<<<<< HEAD
 
 DONT LIKE THIS LOL HAHAH
+=======
+  return (
+    <div className="app-structure-container">
+      <Helmet>
+        <meta
+          name="description"
+          content="TubeStamp is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience."
+   
+this code is bullshit "LOL DONT DO THIS AGAIN JUNIOR DEV"
+>>>>>>> parent of 1215b40 (Revert "YOUR CODE IS L")

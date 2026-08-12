5413cab17dc01e235bd5c0df9c654f6a6026e75c
Lesze <lesterandrew.arollado.cvt@eac.edu.ph>
2026-08-12 22:55:26 +0800

    final testing of learning lol

--- src/App.js
+++ src/App.js
@@ -5,22 +5,4 @@ import LandingPage from './unAuth/LandingPage';
 import './App.css';
 import './firebase';
 
-function App() {
-  return (
-    <div className="app-structure-container">
-      <Helmet>
-        <meta
-          name="description"
-          content="TubeStamp is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience."
-        />
-        <meta
-          name="keywords"
-          content="YouTube, timestamp, links, video, sharing, free, online, tool"
-        />
-      </Helmet>
-      <LandingPage />
-    </div>
-  );
-}
-
-export default App;
+//LOL I AM BACK AT IT AGAIN WAHHWHAHAHH
